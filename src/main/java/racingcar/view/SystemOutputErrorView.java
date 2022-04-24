088d88ee89d61138932848b45732109f4a09b5c4
otakjong <tj3828@naver.com>
2022-04-24 13:58:47 +0900

    test: 작은 단위의 객체부터 테스트코드 작성 (3)
    GameMachine
    
    전체 게임에 대한 테스트 케이스 검사 추가

--- src/main/java/racingcar/view/SystemOutputErrorView.java
+++ src/main/java/racingcar/view/SystemOutputErrorView.java
@@ -2,7 +2,7 @@ package racingcar.view;
 
 public class SystemOutputErrorView {
 
-    private static String ERROR_PREFIX = "[ERROR] ";
+    public static String ERROR_PREFIX = "[ERROR] ";
 
     public static void printError(String message) {
         System.out.println(ERROR_PREFIX + message);
